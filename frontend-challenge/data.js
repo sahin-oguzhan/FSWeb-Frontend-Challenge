c6ece70385371c2696e427eae13114e02ef53d7b
sahin-oguzhan <oguzsahiinn@gmail.com>
2025-06-19 22:00:10 +0300

    Data dosyası ve Componentler

---
+++ frontend-challenge/data.js
@@ -0,0 +1,184 @@
+const data = {
+  en: {
+    heroSection: {
+      name: 'Oğuzhan',
+      greeting: 'Creative thinker Minimalism lover',
+      intro:
+        "Hi, I'm Oğuzhan. I'm a full-stack developer. If you are looking for a Developer who to craft solid and scalable frontend products with great user experiences. Let's shake hands with me.",
+      profileImage: '',
+    },
+    socials: [
+      {
+        name: 'Hire me',
+        logo: '',
+        alt_text: '',
+        link: '',
+      },
+      {
+        name: 'Github',
+        logo: '',
+        alt_text: '',
+        link: '',
+      },
+      {
+        name: 'Linkedin',
+        logo: '',
+        alt_text: '',
+        link: '',
+      },
+    ],
+    skillsSection: {
+      title: 'Skills',
+      skills: [
+        {
+          name: 'JavaScript',
+          icon: '',
+          desc: '',
+        },
+        {
+          name: 'React.js',
+          icon: '',
+          description: '',
+        },
+        {
+          name: 'Node.js',
+          icon: '',
+          desc: '',
+        },
+      ],
+    },
+    profileSection: {
+      title: 'Profile',
+      profile: {
+        name: 'Profile',
+        birthDate: '04.08.1999',
+        location: 'İzmir',
+        education: 'Anadolu Ünv. İktisat Lisans, 2023',
+        role: 'Frontend, Backend, Fullstack',
+      },
+      aboutMe: { name: 'Hakkımda', desc: '' },
+    },
+    projectsSection: [
+      {
+        image: '',
+        name: 'Workintech',
+        desc: '',
+        apps: ['react, redux, axios'],
+        github: 'https://github.com/sahin-oguzhan',
+        view: 'https://github.com/sahin-oguzhan',
+      },
+      {
+        image: '',
+        name: 'Random Jokes',
+        desc: '',
+        apps: ['react, redux, axios'],
+        github: 'https://github.com/sahin-oguzhan',
+        view: 'https://github.com/sahin-oguzhan',
+      },
+      {
+        image: '',
+        name: 'Joruney',
+        desc: '',
+        apps: ['react, redux, axios'],
+        github: 'https://github.com/sahin-oguzhan',
+        view: 'https://github.com/sahin-oguzhan',
+      },
+    ],
+    footerSection: {
+      desc: "Let's work together on your next product.",
+      email: 'oguzsahiinn@gmail.com',
+      social: ['Personal Blog', 'Github', 'Linkedin'],
+    },
+  },
+  tr: {
+    heroSection: {
+      name: 'Oğuzhan',
+      greeting: 'Yaratıcı düşünen ve Minimaliz seven',
+      intro:
+        'Merhaba!, Ben Oğuzhan. Ben bir full-stack geliştiricisiyim. Harika kullanıcı deneyimleri ile sağlam ve ölçeklenebilir frontend ürünleri oluşturacak bir geliştirici arıyorsanız birlikte çalışabiliriz.',
+      profileImage: '',
+    },
+    socials: [
+      {
+        name: 'Bana ulaş',
+        logo: '',
+        alt_text: '',
+        link: '',
+      },
+      {
+        name: 'Github',
+        logo: '',
+        alt_text: '',
+        link: '',
+      },
+      {
+        name: 'Linkedin',
+        logo: '',
+        alt_text: '',
+        link: '',
+      },
+    ],
+    skillsSection: {
+      title: 'Skills',
+      skills: [
+        {
+          name: 'JavaScript',
+          icon: '',
+          desc: '',
+        },
+        {
+          name: 'React.js',
+          icon: '',
+          description: '',
+        },
+        {
+          name: 'Node.js',
+          icon: '',
+          desc: '',
+        },
+      ],
+    },
+    profileSection: {
+      title: 'Profil',
+      profile: {
+        name: 'Profil',
+        birthDate: '04.08.1999',
+        location: 'İzmir',
+        education: 'Anadolu Ünv. İktisat Lisans, 2023',
+        role: 'Frontend, Backend, Fullstack',
+      },
+      aboutMe: { name: 'Hakkımda', desc: '' },
+    },
+    projectsSection: [
+      {
+        image: '',
+        name: 'Workintech',
+        desc: '',
+        apps: ['react, redux, axios'],
+        github: 'https://github.com/sahin-oguzhan',
+        view: 'https://github.com/sahin-oguzhan',
+      },
+      {
+        image: '',
+        name: 'Random Jokes',
+        desc: '',
+        apps: ['react, redux, axios'],
+        github: 'https://github.com/sahin-oguzhan',
+        view: 'https://github.com/sahin-oguzhan',
+      },
+      {
+        image: '',
+        name: 'Joruney',
+        desc: '',
+        apps: ['react, redux, axios'],
+        github: 'https://github.com/sahin-oguzhan',
+        view: 'https://github.com/sahin-oguzhan',
+      },
+    ],
+    footerSection: {
+      desc: 'Bir sonraki projeniz üzerinde birlikte çalışalım.',
+      email: 'oguzsahiinn@gmail.com',
+      social: ['Kişisel Blog', 'Github', 'Linkedin'],
+    },
+  },
+};
